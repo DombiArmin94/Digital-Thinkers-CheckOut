03b88b4a9007667c715370b7a57c4e96231999c3
DombiArmin <adombi@buythesell.com>
2022-07-15 15:09:54 +0200

    Basic Setup of endpoints, and api startup

---
+++ CheckOut/CheckOut/Controllers/CheckoutV1Controller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckOut.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/v1/[action]")]
+    public class CheckoutV1Controller : ControllerBase
+    {
+        protected readonly ILogger _logger;
+
+        public CheckoutV1Controller(ILogger<CheckoutV1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Stock()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Stock(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CheckOut/CheckOut/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace BTS.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
+        {
+            Configuration = configuration;
+            this.logger = logger;
+        }
+
+        public IConfiguration Configuration { get; }
+        private ILogger<Startup> logger;
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}");
+
+                routes.MapRoute(
+                    "NotFound",
+                    "{*.}",
+                    new { controller = "Home", action = "Error" }
+                );
+            });
+        }
+    }
+}
